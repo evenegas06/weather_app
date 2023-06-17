600042cabc5fa9be73ecc7ee85a5e0c36e6db926
Eduardo Venegas <evenegas06@hotmail.com>
2023-06-17 14:16:29 -0600

    Input validations

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,69 @@ import { LoadingButton } from "@mui/lab";
 import { Box, Container, TextField, Typography } from "@mui/material";
 import { useState } from "react";
 
+const API_WEATHER = `https://api.weatherapi.com/v1/current.json?key=${import.meta.env.VITE_API_KEY}&q=`;
+
 function App() {
     /* ----- States ----- */
     const [city, setCity] = useState('');
     const [loading, setLoading] = useState(false);
 
+    const [error, setError] = useState({
+        error: false,
+        message: "",
+    });
+    const [weather, setWeather] = useState({
+        city: '',
+        country: '',
+        temperature: '',
+        condition: '',
+        icon: '',
+        conditionText: ''
+    });
+
     /**
      * Submit form
+     * 
      * @param {Object} event 
      */
-    const handleSubmit = (event) => {
+    const onSubmit = async (event) => {
         event.preventDefault();
-        console.log('Submit');
+
+        setLoading(true);
+        setError({
+            error: false,
+            message: ''
+        });
+
+        try {
+            if (!city.trim()) {
+                throw { message: 'Este campo es obligatorio. 😾' };
+            }
+
+            const response = await fetch(`${API_WEATHER}${city}`);
+            const data = await response.json();
+
+            if (data.error) {
+                throw { message: data.error.message };
+            }
+
+            setWeather({
+                city: data.location.name,
+                country: data.location.country,
+                temperature: data.current.temp_c,
+                condition: data.current.condition.code,
+                icon: data.current.condition.icon,
+                conditionText: data.current.condition.text,
+            });
+        } catch (error) {
+            setError({
+                error: true,
+                message: error.message,
+            });
+            console.error(error);
+        } finally {
+            setLoading(false);
+        }
     };
 
     return (
@@ -22,7 +73,7 @@ function App() {
             sx={{ mt: 2 }}
         >
             <Typography
-                variant="h3"
+                variant="h2"
                 component="h1"
                 align="center"
                 gutterBottom
@@ -34,7 +85,7 @@ function App() {
                 sx={{ display: "grid", gap: 2 }}
                 component="form"
                 autoComplete="off"
-                onSubmit={handleSubmit}
+                onSubmit={onSubmit}
             >
                 <TextField
                     id="city"
@@ -45,6 +96,8 @@ function App() {
                     fullWidth
                     value={city}
                     onChange={(event) => { setCity(event.target.value); }}
+                    error={error.error}
+                    helperText={error.message}
                 />
 
                 <LoadingButton
@@ -55,20 +108,57 @@ function App() {
                 >
                     Buscar
                 </LoadingButton>
-
-                <Typography
-                    textAlign="center"
-                    sx={{ mt: 2, fontSize: "10px" }}
-                >
-                    Powered by:{" "}
-                    <a
-                        href="https://www.weatherapi.com/"
-                        title="Weather API"
-                    >
-                        WeatherAPI.com
-                    </a>
-                </Typography>
             </Box>
+
+            {weather.city && (
+                <Box
+                    sx={{
+                        mt: 2,
+                        display: "grid",
+                        gap: 2,
+                        textAlign: "center"
+                    }}
+                >
+                    <Typography
+                        variant="h3"
+                        component="h2"
+                    >
+                        {weather.city}, {weather.country}
+                    </Typography>
+
+                    <Box
+                        component="img"
+                        alt={weather.conditionText}
+                        src={weather.icon}
+                        sx={{ margin: "0 auto" }}
+                    />
+                    <Typography
+                        variant="h4" component="h3"
+                    >
+                        {weather.temperature} °C
+                    </Typography>
+
+                    <Typography
+                        variant="h5"
+                        component="h4"
+                    >
+                        {weather.conditionText}
+                    </Typography>
+                </Box>
+            )}
+
+            <Typography
+                textAlign="center"
+                sx={{ mt: 2, fontSize: "10px" }}
+            >
+                Powered by:{" "}
+                <a
+                    href="https://www.weatherapi.com/"
+                    title="Weather API"
+                >
+                    WeatherAPI.com
+                </a>
+            </Typography>
         </Container>
     );
 }
